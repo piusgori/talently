aea0d9e3a48a5a30d7622bfceda093f725219e73
piusgori <piusgori@gmail.com>
2024-07-29 21:30:11 +0300

    Insert conversation

---
+++ insert-data.sql
@@ -0,0 +1,47 @@
+-- INSERT INTO employers (
+--     company_name,
+--     company_address,
+--     yearly_revenue,
+--     is_hiring
+-- ) VALUES (
+--     'Learning In',
+--     '1600 Amphitheatre Parkway, Mountain View, CA 94043',
+--     123.12,
+--     TRUE
+-- );
+
+-- INSERT INTO employers (
+--     company_name,
+--     company_address,
+--     yearly_revenue,
+--     is_hiring
+-- ) VALUES (
+--     'Big Oil',
+--     'Slippery Street 110, Houston',
+--     112.55,
+--     FALSE
+-- );
+
+-- INSERT INTO employers (
+--     company_name,
+--     company_address,
+--     yearly_revenue,
+--     is_hiring
+-- ) VALUES (
+--     'Hipster Food',
+--     'Avocade Strees 5, Berlin',
+--     6.12,
+--     TRUE
+-- );
+
+INSERT INTO conversation (
+    user_name,
+    employer_name,
+    message,
+    date_sent
+) VALUES (
+    'Michael Scott',
+    'Learning In',
+    'Hi, I like learning!',
+    '2024-07-29 21:27:00'
+);
