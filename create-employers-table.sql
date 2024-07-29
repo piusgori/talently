bad0924e3f12e6b829434e2c8690a3c717ca72a6
piusgori <piusgori@gmail.com>
2024-07-29 21:04:33 +0300

    Create employers

---
+++ create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers (
+    company_name VARCHAR(250),
+    company_address VARCHAR(300),
+    -- yearly_revenue FLOAT(5, 2), --  Allowed: 123.12, 12.1   Not Allowed: 1234.12, 1.123
+    yearly_revenue NUMERIC(5, 2),    --  Allowed: 123.12   Not Allowed: 12345.123
+    is_hiring BOOLEAN
+);
