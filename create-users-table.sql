dd21defd29a5b25453c7c8d6c01b7d57eef64aba
piusgori <piusgori@gmail.com>
2024-07-29 14:36:22 +0300

    Postgresql enum format

--- create-users-table.sql
+++ create-users-table.sql
@@ -1,5 +1,10 @@
+--  Postgresql + ENUM => create custom type
+
+--  CREATE TYPE employment_status AS ENUM ('employed', 'self-employed', 'unemployed');
+
 CREATE TABLE users (
     full_name VARCHAR(200),
     yearly_salary INT,
     current_status ENUM('employed', 'self-employed', 'unemployed') -- MYSQL syntax
+    -- current_status employment_status -- Postgresql syntax
 );
