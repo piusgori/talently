b996bb6a50e6b6cc9da2b8a49c1a0750583552d4
piusgori <piusgori@gmail.com>
2024-07-30 13:26:25 +0300

    Add constraint

---
+++ 15-check-constraints.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE users (
+--     full_name VARCHAR(300) NOT NULL,
+--     yearly_salary INT CHECK (yearly_salary > 0)
+--     -- CHECK (yearly_salary < max_salary)
+-- );
+
+UPDATE users SET yearly_salary = NULL WHERE full_name = 'Kevin Malone';
+
+ALTER TABLE users
+ADD CONSTRAINT yearly_salary_positive CHECK (yearly_salary > 0);
