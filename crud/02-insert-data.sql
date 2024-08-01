fd9ce7f02405ef06e06af1ab2c8bf87f6e928eb1
piusgori <piusgori@gmail.com>
2024-08-01 11:00:15 +0300

    Insert data

--- crud/02-insert-data.sql
+++ crud/02-insert-data.sql
@@ -1,1 +1,32 @@
-INSERT INTO sales ();
+-- INSERT INTO sales 
+-- (
+--     customer_name, 
+--     product_name, 
+--     volume, 
+--     is_recurring
+-- ) 
+-- VALUES ('Max Schwarz', 'A Book', 12.99, TRUE);
+
+INSERT INTO sales (
+    date_fulfilled,
+    customer_name,
+    product_name,
+    volume,
+    is_recurring,
+    is_disputed
+) VALUES (
+    NULL,
+    'Learning Inc',
+    'Course Bundle',
+    4889.62,
+    FALSE,
+    FALSE
+),
+(
+    '2024-08-01',
+    'Big Oil Inc',
+    'Trucks',
+    400000.0,
+    FALSE,
+    TRUE
+);
