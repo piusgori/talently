df86e72a0c9846185eb715105ac921c372359673
piusgori <piusgori@gmail.com>
2024-08-01 12:02:55 +0300

    Order and limit

---
+++ crud/12-order-and-limit.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM sales
+-- ORDER BY volume DESC
+-- LIMIT 10;
+
+-- SELECT * FROM sales
+-- WHERE is_disputed IS FALSE
+-- ORDER BY volume DESC
+-- LIMIT 3;
+
+SELECT * FROM sales
+ORDER BY volume DESC
+LIMIT 5
+OFFSET 5;
